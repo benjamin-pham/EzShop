e93425ac6c7749bcd5b43150d5aac8ad3f24956f
benjamin <manpham.dev@gmail.com>
2026-09-15 15:58:16 +0700

    chore: update AppHost to wire identity dependencies for core-api and storefront admin

--- aspire/AppHost.cs
+++ aspire/AppHost.cs
@@ -2,19 +2,23 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var webhost = builder.AddProject("core-api", "../apps/core-api/src/EzShop.WebHost/EzShop.WebHost.csproj")
-    .WithReference(cache)
-    .WaitFor(cache)
-    .WithHttpHealthCheck("/health")
-    .WithExternalHttpEndpoints();
-
 var identity = builder.AddProject("identity", "../apps/identity/src/EzShop.Identity.WebHost/EzShop.Identity.WebHost.csproj")
     .WithReference(cache)
     .WaitFor(cache)
     .WithExternalHttpEndpoints();
 
+var coreApi = builder.AddProject("core-api", "../apps/core-api/src/EzShop.WebHost/EzShop.WebHost.csproj")
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(identity)
+    .WaitFor(identity)
+    .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
+
 var storefrontAdmin = builder.AddViteApp("storefront-admin-web", "../apps/storefront-admin-web")
-    .WithReference(webhost)
-    .WaitFor(webhost);
+    .WithReference(coreApi)
+    .WaitFor(coreApi)
+    .WithReference(identity)
+    .WaitFor(identity);
 
 builder.Build().Run();
