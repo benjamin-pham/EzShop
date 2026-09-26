ee18f756e63e96b0de371c00217a5099d96dbc69
benjamin <manpham.dev@gmail.com>
2026-09-26 15:16:19 +0700

    chore(aspire): integrate PostgreSQL resource and wire to apps

--- aspire/AppHost.cs
+++ aspire/AppHost.cs
@@ -1,13 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin()
+    .WithDataVolume();
+var db = postgres.AddDatabase("Database");
+
 var cache = builder.AddRedis("cache");
 
 var identity = builder.AddProject("identity", "../apps/identity/src/EzShop.Identity.WebHost/EzShop.Identity.WebHost.csproj")
+    .WithReference(db)
+    .WaitFor(db)
     .WithReference(cache)
     .WaitFor(cache)
     .WithExternalHttpEndpoints();
 
 var coreApi = builder.AddProject("core-api", "../apps/core-api/src/EzShop.WebHost/EzShop.WebHost.csproj")
+    .WithReference(db)
+    .WaitFor(db)
     .WithReference(cache)
     .WaitFor(cache)
     .WithReference(identity)
