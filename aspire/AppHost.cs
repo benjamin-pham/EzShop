2ac2ff8a1eb392c6ef8f66bb450b0a694fd05d4f
benjamin <manpham.dev@gmail.com>
2026-09-26 15:43:59 +0700

    feat(aspire): add dummy executables for postgres and redis to appear on dashboard

--- aspire/AppHost.cs
+++ aspire/AppHost.cs
@@ -3,9 +3,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 var db = builder.AddConnectionString("Database");
 var cache = builder.AddConnectionString("cache");
 
-// Dummy resource để hiển thị link PgAdmin đang chạy trên Docker lên Aspire Dashboard
+// Các Dummy resource để hiển thị các dịch vụ từ Docker Compose lên Aspire Dashboard
 var pgadminUi = builder.AddExecutable("pgadmin", "sleep", ".", "infinity")
     .WithHttpEndpoint(port: 5050, name: "ui", isProxied: false);
+
+var postgresUi = builder.AddExecutable("postgres", "sleep", ".", "infinity")
+    .WithEndpoint(port: 5434, name: "tcp", scheme: "tcp", isProxied: false);
+
+var redisUi = builder.AddExecutable("redis", "sleep", ".", "infinity")
+    .WithEndpoint(port: 6379, name: "tcp", scheme: "tcp", isProxied: false);
 
 var identity = builder.AddProject("identity", "../apps/identity/src/EzShop.Identity.WebHost/EzShop.Identity.WebHost.csproj")
     .WithReference(db)
